9ad99b1564b0b666ccd4777ee30fbd2bf1e90077
Kirubaharan <kirubakrishkk@gmail.com>
2025-07-28 17:08:41 +0530

    Add files via upload

---
+++ src/Wool_management.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package wool_management;
+
+/**
+ *
+ * @author deepak
+ */
+public class Wool_management {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
